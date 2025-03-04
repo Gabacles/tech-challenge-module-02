30b03f8c8bef4599fa9d1af79314ea81092a30d9
Gabacles <g.dalrovere@gmail.com>
2025-03-04 18:09:53 -0300

    feat: add user routes for creating a user

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,8 @@ import "reflect-metadata";
 import "@/lib/typeorm/typeorm";
 import fastify from "fastify";
 
+import { userRoutes } from "./http/controllers/user/routes";
+
 export const app = fastify();
+
+app.register(userRoutes);
