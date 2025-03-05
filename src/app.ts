a5c898de5c0760542bdf1503375d3107e384657b
Gabacles <g.dalrovere@gmail.com>
2025-03-04 22:51:52 -0300

    feat: integrate JWT authentication middleware with Fastify

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,20 @@ import { globalErrorHandler } from "./utils/global-error-handler";
 
 import { userRoutes } from "./http/controllers/user/routes";
 import { postRoutes } from "./http/controllers/post/routes";
+import fastifyJwt from "@fastify/jwt";
+import { env } from "./env";
+import { jwtValidate } from "./http/controllers/middlewares/jwt-validate";
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  sign: {
+    expiresIn: "30m",
+  },
+});
+
+app.addHook("onRequest", jwtValidate);
 
 app.register(userRoutes);
 app.register(postRoutes);
