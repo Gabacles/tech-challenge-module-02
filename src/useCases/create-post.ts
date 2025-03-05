6c5ebdb0b51426412f7b8c8c749b02cc69ca5e3a
Gabacles <g.dalrovere@gmail.com>
2025-03-05 10:40:07 -0300

    refactor: remove role validation from create post use case

--- src/useCases/create-post.ts
+++ src/useCases/create-post.ts
@@ -1,20 +1,10 @@
 import { IPost } from "@/entities/models/post.interface";
 import { IPostRepository } from "@/repositories/models/post.repository.interface";
-import { RoleNotAllowedError } from "./errors/role-not-allowed-error";
-import { UserRepository } from "@/repositories/user.repository";
-import { UserRole } from "@/entities/models/userRoles.enum";
 
 export class CreatePostUseCase {
   constructor(private postRepository: IPostRepository) {}
 
   async handler(post: IPost): Promise<IPost | undefined> {
-    const userRepository = new UserRepository();
-    const user = await userRepository.findUserById(post.author_id);
-
-    if (user?.roles !== UserRole.TEACHER) {
-      throw new RoleNotAllowedError();
-    }
-
     return this.postRepository.createPost(post);
   }
 }
