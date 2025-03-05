caca5cb73f7ae7f026214ad0f43fd67bc18b1caf
Gabacles <g.dalrovere@gmail.com>
2025-03-05 10:44:15 -0300

    feat: update create post handler to use user ID from request token

--- src/http/controllers/post/create.ts
+++ src/http/controllers/post/create.ts
@@ -6,14 +6,23 @@ export async function create(req: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     title: z.string(),
     content: z.string(),
-    author_id: z.string(),
   });
 
-  const { title, content, author_id } = registerBodySchema.parse(req.body);
+  const registerTokenSchema = z.object({
+    id: z.string(),
+  });
+
+  const { id } = registerTokenSchema.parse(req.user);
+
+  const { title, content } = registerBodySchema.parse(req.body);
 
   const createPostUseCase = makeCreatePostUseCase();
 
-  const post = await createPostUseCase.handler({ title, content, author_id });
+  const post = await createPostUseCase.handler({
+    title,
+    content,
+    author_id: id,
+  });
 
   return reply.code(201).send({ id: post?.id, title: post?.title });
 }
