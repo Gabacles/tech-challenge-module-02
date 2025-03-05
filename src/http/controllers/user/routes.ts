7f7cf02c8a6610bcf40519f6d95e4a38eb31b615
Gabacles <g.dalrovere@gmail.com>
2025-03-04 22:52:25 -0300

    feat: add signin route and implement signin use case for user authentication

--- src/http/controllers/user/routes.ts
+++ src/http/controllers/user/routes.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from "fastify";
 import { create } from "./create";
 import { findUserById } from "./find-user";
 import { findAllUsers } from "./find-all-users";
+import { signin } from "./signin";
 
 export async function userRoutes(app: FastifyInstance) {
   app.post("/user", create);
   app.get("/user/:id", findUserById);
   app.get("/user", findAllUsers);
+  app.post("/user/signin", signin);
 }
---
+++ src/http/controllers/user/signin.ts
@@ -0,0 +1,29 @@
+
+import { invalidCredentialsError } from "@/useCases/errors/invalid-credentials-error";
+import { makeSigninUseCase } from "@/useCases/factory/make-signin-use-case";
+import { compare } from "bcryptjs";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function signin(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    email: z.string(),
+    password: z.string(),
+  });
+
+  const { email, password } = registerBodySchema.parse(request.body);
+
+  const signinUseCase = makeSigninUseCase();
+
+  const user = await signinUseCase.handler(email);
+
+  const doestPasswordMatch = await compare(password, user.passwordHash);
+
+  if (!doestPasswordMatch) {
+    throw new invalidCredentialsError();
+  }
+
+  const token = await reply.jwtSign({ email });
+
+  return reply.status(200).send({ token });
+}
